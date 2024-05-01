d21bf110a725085fc489e9b76dec748910d5b9d0
Luis Carballo <lcarballo@next-cloud.mx>
2024-05-01 13:48:22 -0600

    Primer commit con los cambios

---
+++ comments/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from comments.models import Comment
+from users.api.serializers import UserSerializer
+from posts.api.serializers import PostSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """user = UserSerializer()
+    post = PostSerializer()"""
+
+    class Meta:
+        model = Comment
+        fields = ('content', 'created_at', 'user', 'post',)
